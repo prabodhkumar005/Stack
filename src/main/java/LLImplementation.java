fc42be4edd6fbaa481d3eb92a1fca7869f195865
prabodhkumar005 <prabodhkumar7254@gmail.com>
2024-02-14 14:42:15 +0530

    added constructor for the Node class

--- src/main/java/LLImplementation.java
+++ src/main/java/LLImplementation.java
@@ -6,7 +6,18 @@ public class LLImplementation {
             this.val=val;
         }
     }
+    public static class LLStack{
+        Node head=null;
+        int size=0;
+        void push(int x){
+            Node temp=new Node(x);
+            temp.next=head;
+            head=temp;
+            size++;
+        }
+    }
     public static void main(String[] args) {
+
         Stack st=new Stack();
     }
 }
