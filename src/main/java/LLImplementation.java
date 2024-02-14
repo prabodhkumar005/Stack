f2eaa5fdd44091744b654c7fb9885fe646465cc9
prabodhkumar005 <prabodhkumar7254@gmail.com>
2024-02-14 14:35:09 +0530

    created the Node class

--- src/main/java/LLImplementation.java
+++ src/main/java/LLImplementation.java
@@ -1,5 +1,12 @@
 public class LLImplementation {
+    public static class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val=val;
+        }
+    }
     public static void main(String[] args) {
-
+        Stack st=new Stack();
     }
 }
