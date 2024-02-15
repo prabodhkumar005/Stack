2c918533d3551879fff0aded99f2805856059490
prabodhkumar005 <prabodhkumar7254@gmail.com>
2024-02-16 00:43:33 +0530

    created a function isBalanced()

--- src/main/java/balancedBrackets.java
+++ src/main/java/balancedBrackets.java
@@ -1,5 +1,12 @@
-public class balancedBrackets {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class balancedBrackets {
+    public static boolean isBalanced(String str){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(isBalanced(str));
     }
 }
